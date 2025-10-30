a68280f6dd7a65c0fa843506a85347383996a026
Shankar <100808639+babyshank09@users.noreply.github.com>
2025-10-31 00:45:28 +0530

    Delete tests directory

--- tests/api_test.py
+++
@@ -1,9 +0,0 @@
-import dotenv 
-from dotenv import load_dotenv  
-import os
-
-load_dotenv("api.env") 
-
-# serper_api_key = = os.getenv("SERPER_API_KEY") 
-serper_api_key = os.getenv("SERPER_API_KEY")
-print(serper_api_key)
